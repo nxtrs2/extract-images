432c240508d6a04aa43e57523636496070543ac5
nxtrs2 <99115200+nxtrs2@users.noreply.github.com>
2024-02-17 22:01:20 +0500

    Update CORS configuration in app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,24 @@ const cors = require("cors");
 const app = express();
 const port = 3030;
 
-app.use(cors());
+const allowedOrigins = [
+  "https://www.extractimages.io",
+  "https://extractimages.io",
+  "http://localhost:3000",
+];
+
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  let origin = req.header("Origin");
+  if (allowedOrigins.includes(origin)) {
+    corsOptions = { origin: true }; // Reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // Disable CORS for this request
+  }
+  callback(null, corsOptions); // Callback expects two parameters: error and options
+};
+
+app.use(cors(corsOptionsDelegate));
 
 const uploadRoutes = require("./routes/uploadRoutes");
 const imageRoutes = require("./routes/imagesRoutes");
